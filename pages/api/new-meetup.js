c4269e0e86eea6e94fc47c0d6e1cf1ddd4d31f98
yofan2408 <yofan.ixe@gmail.com>
2021-11-29 22:44:41 +0700

    first commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+
+async function handler(req, res) {
+    if (req.method === 'POST') {
+      const data = req.body;
+  
+      const client = await MongoClient.connect(
+        'mongodb://localhost:27017/meetups'
+      );
+      const db = client.db();
+  
+      const meetupsCollection = db.collection('meetups');
+  
+      const result = await meetupsCollection.insertOne(data);
+  
+      console.log(result);
+  
+      client.close();
+  
+      res.status(201).json({ message: 'Meetup inserted!' });
+    }
+  }
+  
+  export default handler;
